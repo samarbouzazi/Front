6edd8aadde54927612be83538aa82ac3e8531242
samarbouzezi <samar.bouzezi@esprit.tn>
2023-04-30 17:46:13 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddAnimalComponent } from './FrontOffice/Animals/add-animal/add-animal.component';
+import { ViewAnimalsComponent } from './FrontOffice/Animals/view-animals/view-animals.component';
+import { UpdateAnimalComponent } from './FrontOffice/Animals/update-animal/update-animal.component';
+
+import { FooterComponent } from './FrontOffice/shared/footer/footer.component';
+import { HomeComponent } from './FrontOffice/home/home.component';
+import { LoginComponent } from './FrontOffice/User/login/login.component';
+import { RegisterComponent } from './FrontOffice/User/register/register.component';
+import { NavbarComponent } from './BackOffice/navbar/navbar.component';
+import { FooterBackEndComponent } from './BackOffice/footerBackEnd/footer-back-end/footer-back-end.component';
+import { AdminBoardComponent } from './BackOffice/admin-board/admin-board.component';
+import { AccountComponent } from './FrontOffice/User/Account/account/account.component';
+import { HeaderComponent } from './FrontOffice/shared/header/header.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AddAnimalComponent,
+        ViewAnimalsComponent,
+        UpdateAnimalComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        NavbarComponent,
+      FooterBackEndComponent,
+      AdminBoardComponent,
+      AccountComponent
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
